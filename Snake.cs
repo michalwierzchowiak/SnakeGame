a8c50bc7fdaec82db21da8e6d6557c0199db0453
michalwierzchowiak <chx101443@student.chorzow.merito.pl>
2026-01-11 13:31:32 +0100

    Poprawiono warunek z przyrównaniem yPos przy Hindernis treffen

--- Snake.cs
+++ Snake.cs
@@ -212,7 +212,7 @@ class Program
 
             //Hindernis treffen
 
-            if (hoofd.xPos == obstacleXpos /* ?? */ == obstacleYpos)
+            if (hoofd.xPos == obstacleXpos && hoofd.yPos == obstacleYpos)
 
             {
 
